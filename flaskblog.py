4264fc2bc63e49137e49ed40d0adfc3ecc890ee1
weesheng.chu <weesheng10@gmail.com>
2020-02-18 16:06:34 +0800

    first commit

---
+++ flaskblog.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4453f2d45490c06c5d8abf1ab6555908'
+
+posts = [
+    {
+        'author': 'Wee Sheng',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Feb 5, 2020'
+    },
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 20, 2018'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+# allow us to run 'python flaskblog.py' instead of 'flask run'
+if __name__ == '__main__':
+    app.run(debug=True)
