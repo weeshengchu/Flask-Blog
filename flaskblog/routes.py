139c39920e9879402ea40b8a8ea1e95b8b64b1c9
weesheng.chu <weesheng10@gmail.com>
2020-02-22 07:26:00 +0800

    create, update and delete post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,31 +1,33 @@
 import os
 import secrets
 from PIL import Image
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author': 'Wee Sheng',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Feb 5, 2020'
-    },
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 20, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Wee Sheng',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'Feb 5, 2020'
+#     },
+#     {
+#         'author': 'Corey Schafer',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'April 20, 2018'
+#     }
+# ]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
+    # 8. displaying post
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -80,7 +82,7 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-# 7. import secrets + os
+# 7. import secrets + os to generate random hex for saved pictures
 
 
 def save_picture(form_picture):
@@ -121,3 +123,57 @@ def account():
     image_file = url_for(
         'static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+# 8. CUD posts
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        # 8. Saving updated post to home page
+        post = Post(title=form.title.data,
+                    content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        # 8. return 403 page when /3/update
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        # 8. updating the post
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+# 8. deleting the post
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
